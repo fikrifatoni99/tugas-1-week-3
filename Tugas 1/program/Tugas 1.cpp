80373624de3243ea0329585a3bd638e02806b7e5
fikrifatoni99 <43851520+fikrifatoni99@users.noreply.github.com>
2018-10-12 05:26:24 +0700

    Add files via upload

---
+++ Tugas 1/program/Tugas 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+float get_nilai(float a, float b) {
+return (a + b) / 2;
+}
+int main (){
+{
+// definisi struktur
+struct Mahasiswa
+{
+char nim[10]; char nama[30]; float uts; float uas;
+};
+
+Mahasiswa mhs;
+cout<<"				   ENTRI DATA					"<<endl;
+cout << "Masukkan NIM : "; cin.getline(mhs.nim, 10);
+cout << "Masukkan Nama : "; cin.getline(mhs.nama, 30);
+cout << "UTS : "; cin >> mhs.uts; cout << "UAS : "; cin >> mhs.uas;
+cout<<"				   TAMPIL DATA					"<<endl;
+cout << "\nNIM : " << mhs.nim;
+cout << "\nNama : " << mhs.nama;
+cout << "\nNilai : " << get_nilai(mhs.uts, mhs.uas);
+
+getch();
+}
+}
